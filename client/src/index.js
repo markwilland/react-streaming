a91306ba4aafb17f4838a32b1ba7dbd5fcb1bb6e
markwilland <markwillanderson@gmail.com>
2021-05-07 23:39:04 -0400

    removed thunk boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore , applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.querySelector('#root')
-);
+import App from './components/App';
+
+
+
+ReactDOM.render(<App />, document.querySelector("#root"))
