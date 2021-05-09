fc73ea227a4d02952575de5b9fdf142b13ae0f51
markwilland <markwillanderson@gmail.com>
2021-05-08 23:49:46 -0400

    fixed boilerplayte

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const App = () => {
-    return <div>App</div>
-};
-
-
 
 ReactDOM.render(<App />, document.querySelector("#root"))
