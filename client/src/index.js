287ddfb9737b07ba490d71ac29aa45a4d3cdaae2
markwilland <markwillanderson@gmail.com>
2021-05-08 23:46:31 -0400

    added app

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+const App = () => {
+    return <div>App</div>
+};
+
 
 
 ReactDOM.render(<App />, document.querySelector("#root"))
