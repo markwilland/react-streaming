c8de4b7ea9817ac90501f03948c4d57aed8d0f4a
markwilland <markwillanderson@gmail.com>
2021-05-09 21:18:33 -0400

    trivial use of react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            <div>PageOne</div>
+            <Link to="/pagetwo">Navigate to page 2!!!</Link>
+        </div>
+    )
+}
+
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <Link to="/">Navigate to page ONE!!!</Link>
+            <button>Click Me!</button> 
+        </div>
+    )
+}
 
 function App() {
     return (
         <div>
-            App
-        </div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div> 
     )
 }
 
