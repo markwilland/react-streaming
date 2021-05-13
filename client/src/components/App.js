2346858298c32b0e0803ad9e8da128a8ea30a319
markwilland <markwillanderson@gmail.com>
2021-05-12 21:34:26 -0400

    added basic header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
+
 import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
+import Header from './Header'
 
-function App() {
+const App = () => {
     return (
-        <div>
-            <h1>Hi</h1>
+        <div className="ui container">
             <BrowserRouter>
+                <Header />
                 <div>
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" component={StreamCreate} />
